60de6ba161f45bf512488f863ffbc349a1ac3cd5
wangzhibin <wangzhibin@xiaomi.com>
2017-04-06 20:22:28 +0800

    filter and sample

---
+++ app/src/main/java/myproject/com/myapp/RXJavaTest/RxFilter.java
@@ -0,0 +1,69 @@
+package myproject.com.myapp.RXJavaTest;
+
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by wang on 06/04/17.
+ */
+
+public class RxFilter {
+    private static final String TAG = "RxFilter";
+
+    /**
+     * 满足某个条件的事件才会发送给下游 丢失了大部分的事件
+     */
+    public static void filterTest() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                for (int i = 0; ; i++) {
+                    e.onNext(i);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        return integer % 100 == 0;
+                    }
+                }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "accept: " + integer);
+                    }
+                });
+    }
+
+    /**
+     * 每隔一段时间从上游取一个事件发送给下游 丢失了大部分的事件
+     */
+    public static void sampleTest() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                for (int i = 0; ; i++) {
+                    e.onNext(i);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+                .sample(2, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "accept: " + integer);
+                    }
+                });
+    }
+}
