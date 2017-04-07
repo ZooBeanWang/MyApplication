b5d041147572570e4aafb3db1c3f52f51d56285c
wangzhibin <wangzhibin@xiaomi.com>
2017-04-07 16:45:52 +0800

    test flowable

--- app/src/main/java/myproject/com/myapp/RXJavaTest/RxFlowableActivity.java
+++ app/src/main/java/myproject/com/myapp/RXJavaTest/RxFlowableActivity.java
@@ -24,6 +24,8 @@ public class RxFlowableActivity extends AppCompatActivity {
     private Button btn_start;
     private Button btn_request;
     private Subscription subscription;
+    private Flowable<Integer> flowable = null;
+    private Subscriber<Integer> subscriber = null;
 
     public static void startRxFlowableActivity(Context context) {
         Intent intent = new Intent(context, RxFlowableActivity.class);
@@ -37,16 +39,29 @@ public class RxFlowableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rx_flowable);
         btn_start = (Button) findViewById(R.id.btn_start);
         btn_request = (Button) findViewById(R.id.btn_request);
-        final Flowable<Integer> flowable = Flowable.create(new FlowableOnSubscribe<Integer>() {
+        init1();
+
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flowable.observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(subscriber);
+            }
+        });
+
+    }
+
+    private void init1() {
+        flowable = Flowable.create(new FlowableOnSubscribe<Integer>() {
             @Override
             public void subscribe(FlowableEmitter<Integer> e) throws Exception {
                 for (int i = 0; i < 127; i++) {
-                    Log.d(TAG, "subscribe: "+i);
+                    Log.d(TAG, "subscribe: " + i);
                     e.onNext(i);
                 }
             }
         }, BackpressureStrategy.ERROR).subscribeOn(Schedulers.io());
-        final Subscriber<Integer> subscriber = new Subscriber<Integer>() {
+        subscriber = new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription s) {
                 subscription = s;
@@ -67,18 +82,131 @@ public class RxFlowableActivity extends AppCompatActivity {
                 Log.d(TAG, "onComplete: ");
             }
         };
-
-        btn_start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                flowable.observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(subscriber);
-            }
-        });
         btn_request.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 subscription.request(1);
+            }
+        });
+    }
+
+    /**
+     * 可以存储的事件没有大小限制，如果下游不处理还是可能会抛OOM 的
+     */
+    private void init2() {
+        flowable = Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                for (int i = 0; ; i++) {
+                    Log.d(TAG, "subscribe: " + i);
+                    e.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.BUFFER).subscribeOn(Schedulers.io());
+        subscriber = new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                subscription = s;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.e(TAG, "onError: ", t);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        };
+        btn_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subscription.request(128);
+            }
+        });
+    }
+
+    //Drop就是直接把存不下的事件丢弃,Latest就是只保留最新的事件
+    private void init3() {
+        flowable = Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                for (int i = 0; ; i++) {
+                    Log.d(TAG, "subscribe: " + i);
+                    e.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.DROP).subscribeOn(Schedulers.io());
+        subscriber = new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                subscription = s;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.e(TAG, "onError: ", t);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        };
+        btn_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subscription.request(128);
+            }
+        });
+    }
+
+    private void init4() {
+        flowable = Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                for (int i = 0; ; i++) {
+                    Log.d(TAG, "subscribe: " + i);
+                    e.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.LATEST).subscribeOn(Schedulers.io());
+        subscriber = new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                subscription = s;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.e(TAG, "onError: ", t);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        };
+        btn_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subscription.request(128);
             }
         });
     }
